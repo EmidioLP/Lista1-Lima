b46f2f4d80739f3e81b4237e9d4b7bd119f21990
Emidio Lopes <37850911+EmidioLP@users.noreply.github.com>
2018-10-26 17:01:37 -0300

    Create 1.0.cpp

---
+++ 1.0.cpp
@@ -0,0 +1,12 @@
+//Usando vetores: 
+//Uma pessoa deseja armazenar a quanto tempo cada trabalhador trabalha em sua empresa.
+//e em seguida saber quem trabalha a mais tempo e a menos tempo.
+
+	int a[n]; // "n" será a quantidade de elementos do vetor.
+	int maior, menor;
+
+//Usando matrizes: 
+//Uma professora precisa armazenar a nota de 4 alunos que ficaram em recuperação, 
+//3 notas das unidades e a nota da recuperação, assim ela vai precisar de uma matriz 4x4.
+
+   	float M[4][4];
